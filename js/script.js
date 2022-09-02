245d7fef8b5fb3e89b9b1fcd43a7edda52627cb3
Serg-95 <belparus2@gmail.com>
2022-09-02 15:02:38 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+"use strict"
+
+const customersSwiper = document.querySelector('.swiper-customers')
+
+if (customersSwiper) {
+    const swiper = new Swiper('.swiper-customers', {
+        // Optional parameters
+        autoHeight: true,
+        loop: true,
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+      });
+}
